32076ad56e404b0fefe1aec799ce502acddd3fde
Axl Orellana <aorellana@smartdream.es>
2022-02-26 17:29:56 -0500

    update main

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:starwars/src/models/character.dart';
-import 'package:starwars/src/models/gender.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:starwars/src/widgets/widgets.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const ProviderScope(child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
@@ -16,94 +16,14 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xffa44cd3)),
       ),
-      home: const HomePage(),
-    );
-  }
-}
-
-class HomePage extends StatelessWidget {
-  const HomePage({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Starwars Characters'),
-        centerTitle: true,
-        actions: [
-          PopupMenuButton<void>(
-            icon: const Icon(Icons.filter_alt),
-            itemBuilder: (_) => [
-              const PopupMenuItem(child: Text('All')),
-              const PopupMenuItem(child: Text('Male')),
-              const PopupMenuItem(child: Text('Female')),
-              const PopupMenuItem(child: Text('N/A')),
-            ],
-          ),
-        ],
-      ),
-      body: ListView.builder(
-        padding: const EdgeInsets.all(8),
-        itemBuilder: (_, index) => Card(
-          child: ExpansionTile(
-            title: Text(
-              character.name,
-              style: Theme.of(context).textTheme.headline6,
-            ),
-            subtitle: GenderChip(gender: character.gender),
-            childrenPadding: const EdgeInsets.all(8),
-            children: [
-              Text(
-                'films cast',
-                style: Theme.of(context).textTheme.subtitle1,
-              ),
-              for (final movie in character.movies)
-                Chip(
-                  label: Text(movie),
-                ),
-            ],
-          ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Starwars Characters'),
+          centerTitle: true,
+          actions: const [FilterButton()],
         ),
-        itemCount: 5,
+        body: const CharacterList(),
       ),
     );
   }
 }
-
-class GenderChip extends StatelessWidget {
-  const GenderChip({
-    Key? key,
-    required this.gender,
-  }) : super(key: key);
-
-  final Gender gender;
-
-  @override
-  Widget build(BuildContext context) {
-    return Align(
-      alignment: Alignment.centerLeft,
-      child: Chip(
-        label: Text(
-          gender.toString(),
-          style: const TextStyle(color: Colors.white),
-        ),
-        backgroundColor: gender.map(
-          male: (_) => const Color(0xff0f4d92),
-          female: (_) => const Color(0xfff64a8a),
-          na: (_) => const Color(0xffff7518),
-        ),
-      ),
-    );
-  }
-}
-
-const character = Character(
-  name: 'Luke Skywalker',
-  gender: Gender.male(),
-  movies: [
-    'https://swapi.dev/api/films/1/',
-    'https://swapi.dev/api/films/2/',
-    'https://swapi.dev/api/films/3/',
-    'https://swapi.dev/api/films/6/'
-  ],
-);
